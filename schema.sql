5fd27fa2708afd3baf389c0f84a0c1fbefd66b9f
auom.eisiad <daisiemouaa@gmail.com>
2023-11-30 18:19:50 -0500

    fixed some syntax issues

--- schema.sql
+++ schema.sql
@@ -13,23 +13,24 @@ CREATE TABLE role (
     salary DECIMAL,
     department_id INT,
     FOREIGN KEY (department_id) 
-    REFERENCES departments(id)
+    REFERENCES department(id)
     ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name: VARCHAR(30) NOT NULL,
-    last_name: VARCHAR(30) NOT NULL,
+    first_name VARCHAR(30) NOT NULL, 
+    last_name VARCHAR(30) NOT NULL,  
     role_id INT,
     FOREIGN KEY (role_id)
-    REFERENCES roles(id)
-    ON DELETE SET NULL
-    manager_id INT
+    REFERENCES role(id) 
+    ON DELETE SET NULL,
+    manager_id INT,  -- Added comma
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
     ON DELETE SET NULL,
     department_id INT,
     FOREIGN KEY (department_id) 
-    REFERENCES departments(id)
+    REFERENCES department(id)
     ON DELETE SET NULL
-    
 );
